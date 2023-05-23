d48ea3dc6dccc71c66fdaadf0468c65750dd38c2
Zane Falcao <112317470+falcaozane@users.noreply.github.com>
2023-05-23 11:03:36 +0530

    marketplace setup

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
-import React from 'react';
-import { BrowserRouter as Router,Routes,Route  } from 'react-router-dom';
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Loginpage from './Components/Loginpage';
-import Signuppage from'./Components/Signuppage';
-import Homepage from './Components/Homepage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Navbar from './components/Navbar'
+import PrivateRoute from './components/PrivateRoute'
+import Explore from './pages/Explore'
+import Offers from './pages/Offers'
+import Category from './pages/Category'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
+import CreateListing from './pages/CreateListing'
+import EditListing from './pages/EditListing'
+import Listing from './pages/Listing'
+import Contact from './pages/Contact'
 
-import Fetch from './Components/fetch';
-import Upload from './Components/Upload';
-// import Imageupload from './Components/Imageupload';
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Homepage/>}/>
-        <Route path='/Loginpage' element={<Loginpage/>}/>
-        <Route path='/Signuppage' element={<Signuppage/>}/>
-        <Route path='/Fetch' element={<Fetch/>}/>
-        <Route path='/Upload' element={<Upload/>}/>
-        {/* <Route path='/Imageupload' element={<Imageupload/>}/> */}
-        
-        
-      </Routes>
-    </Router>
-  );
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/category/:categoryName' element={<Category />} />
+          <Route path='/profile' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/create-listing' element={<CreateListing />} />
+          <Route path='/edit-listing/:listingId' element={<EditListing />} />
+          <Route
+            path='/category/:categoryName/:listingId'
+            element={<Listing />}
+          />
+          <Route path='/contact/:landlordId' element={<Contact />} />
+        </Routes>
+        <Navbar />
+      </Router>
+
+      <ToastContainer />
+    </>
+  )
 }
 
-export default App;
+export default App
